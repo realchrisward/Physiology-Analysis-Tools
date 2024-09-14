3d5ca643deba7c32883341f28b9034f001c44bb2
Chris Ward <ward.chris.s@gmail.com>
2024-09-13 19:41:11 -0500

    simplified output file, error checking for no output dir and no beat list

--- src/physiology_analysis_tools/main.py
+++ src/physiology_analysis_tools/main.py
@@ -95,6 +95,9 @@ class MainWindow(QtWidgets.QMainWindow):
         )
         self.label_Title_and_Version.setText(f'ECG Analysis - {__version__}')
         self.plotted_counter = 0
+        self.beat_df = None
+        self.output_dir = None
+        # self.beat_df = None # !!! get rid of beat_df...just use beat_df?
         self.time_column = None
         self.voltage_column = None
         self.line = None
@@ -620,14 +623,14 @@ class MainWindow(QtWidgets.QMainWindow):
         self.current_arrhythmia = None
         
         
-        self.arrhythmia_df = arrhythmia_detection.call_arrhythmias(
+        self.beat_df = arrhythmia_detection.call_arrhythmias(
             self.beat_df,
             arrhythmia_detection.Settings()
         )
-        print(self.arrhythmia_df)
+        print(self.beat_df)
         
-        self.arrhythmia_only_df = self.arrhythmia_df[
-            self.arrhythmia_df.any_arrhythmia
+        self.arrhythmia_only_df = self.beat_df[
+            self.beat_df.any_arrhythmia
             ].reset_index()
         
         self.arrhythmia_markers = self.add_plot(
@@ -678,9 +681,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.action_update_current_arrhythmia()
         
     def action_confirm_arrhythmia(self):
-        self.arrhythmia_df.loc[
+        self.beat_df.loc[
             (
-                self.arrhythmia_df['ts'] 
+                self.beat_df['ts'] 
                 == self.arrhythmia_only_df.iloc[
                     self.current_arrhythmia_index
                     ]['ts']
@@ -698,9 +701,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.action_next_arrhythmia()
         
     def action_reject_arrhythmia(self):
-        self.arrhythmia_df.loc[
+        self.beat_df.loc[
             (
-                self.arrhythmia_df['ts'] 
+                self.beat_df['ts'] 
                 == self.arrhythmia_only_df.iloc[
                     self.current_arrhythmia_index
                     ]['ts']
@@ -770,6 +773,14 @@ class MainWindow(QtWidgets.QMainWindow):
     
     def action_generate_report(self):
         print('generate_report')
+        if self.beat_df is None:
+            print('no beat info - did you perform beat and arrhtyhmia detection')
+            return
+        
+        if self.output_dir is None:
+            print('no output dir set')
+            return
+        
         output_path = os.path.join(
             self.output_dir,
             os.path.splitext(
@@ -782,7 +793,7 @@ class MainWindow(QtWidgets.QMainWindow):
             engine='xlsxwriter'
         )
         self.beat_df.to_excel(writer, 'beats', index=False)
-        self.arrhythmia_df.to_excel(writer, 'arrhyth')
+        # self.beat_df.to_excel(writer, 'arrhyth')
         writer.close()
         print('finished')
         
