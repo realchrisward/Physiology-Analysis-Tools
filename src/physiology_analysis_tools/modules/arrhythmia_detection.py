c7229a3d1cf4f56b8d1fdef102679d48af3d1141
Christopher S Ward <34755351+realchrisward@users.noreply.github.com>
2024-09-17 04:34:41 -0500

    Revert "features for marking new arryhtmias etc"
    This reverts commit f5ae14bcb47b2ab374465d0f00ea4306f05688b2.

--- src/physiology_analysis_tools/modules/arrhythmia_detection.py
+++ src/physiology_analysis_tools/modules/arrhythmia_detection.py
@@ -5,7 +5,7 @@ arrhythmia_detection for ECG Analysis Tool
 written by Christopher S Ward (C) 2024
 """
 
-__verison__ = "0.0.3"
+__verison__ = "0.0.2"
 
 # %% import libraries
 import scipy
@@ -14,20 +14,6 @@ import numpy
 
 
 # %% define functions
-
-arrhythmia_categories = [
-        'bradycardia_absolute',
-        'tachycardia_absolute',
-        'skipped_beat',
-        'prem_beat',
-        'any_arrhythmia',
-        'other_arrhythmia'
-    ]
-
-annot_arrhythmia_categories = [
-    f'annot_{i}' for i in arrhythmia_categories
-]
-
 
 class Settings():
     def __init__(self):
@@ -51,7 +37,7 @@ def call_premature_beat_multiple(df,rr_column_name,ts_column_name,threshold):
     return df[ts_column_name].diff()/df[rr_column_name] <= threshold
 
     
-def call_arrhythmias(df,settings,arrhythmia_categories):
+def call_arrhythmias(df,settings):
     
     df['bradycardia_absolute'] = call_bradycardia_absolute(
         df, 'RR', 60/settings.bradycardia_absolute_hr
@@ -65,10 +51,15 @@ def call_arrhythmias(df,settings,arrhythmia_categories):
     df['prem_beat'] = call_premature_beat_multiple(
         df, 'RR','ts', settings.premature_beat_multiple_rr
     )
-    df['any_arrhythmia'] = df[['bradycardia_absolute','tachycardia_absolute','skipped_beat','prem_beat']].any(axis=1,bool_only=True)
-
-    df['other_arrhythmia'] = False
-        
+    df['any_arrhythmia'] = df.any(axis=1,bool_only=True)
+    
+    arrhythmia_categories = [
+        'bradycardia_absolute',
+        'tachycardia_absolute',
+        'skipped_beat',
+        'prem_beat',
+        'any_arrhythmia'
+    ]
     
     for a in arrhythmia_categories:
         df[f'annot_{a}'] = df[a].astype(int)
